cec96192a0e8ccaafd2755461287bf0c6e65e079
JumaOchi <ochiengjuma95@gmail.com>
2023-02-03 00:21:47 +0000

    concatinatinng strins

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+
 '''
-Concatinatin two strings together module 1.
+    Concatinatin two strings together module 1.
 '''
 
 
 def concat(str1: str, str2: str) -> str:
     '''
-    Function returning the Concatenatedtwo strings.
+        Function returning the Concatenatedtwo strings.
     '''
     return str1 + str2
