b951a391f6076545796ea50a3ba37508dde995c7
JumaOchi <ochiengjuma95@gmail.com>
2023-02-08 22:39:33 +0000

    asyncio basic task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-#asyncio
-
+'''Asyncio basic task 0'''
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a randdom number of seconds"""
+    """Waits for a randdom number of seconds
+    """
     wait_t = random.random() * max_delay
     await asyncio.sleep(wait_t)
     return wait_t
